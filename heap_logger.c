d795b3dcb4550c22e3c80700e2675ed790b4928a
matt2905 <matthias2905@hotmail.fr>
2022-12-20 10:24:43 +0100

    better formatating

--- heap_logger.c
+++ heap_logger.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief Function to emptying the log file
+ *
+ */
 void clean_log(void)
 {
     FILE *log_file = fopen("log", "w");
@@ -14,31 +18,41 @@ void clean_log(void)
     fclose(log_file);
 }
 
+/**
+ * @brief Function that log the different event of heap_alloc
+ *
+ * @param action
+ * @param size
+ * @param ptr
+ */
 void write_log(char *action, size_t size, void *ptr)
 {
     time_t timestamp = time(NULL);
     struct tm *time_infos = localtime(&timestamp);
     FILE *log_file = fopen("log", "a");
+    char time_buffer[80];
 
     if (log_file == NULL)
     {
         fprintf(stderr, "write logger can't open log file\n");
         exit(0);
     }
-    fprintf(log_file, "[%02d/%02d/%04d][%02d:%02d:%02d] %s %ld bytes at address %p\n",
-            time_infos->tm_mday, time_infos->tm_mon + 1, time_infos->tm_year + 1900,
-            time_infos->tm_hour, time_infos->tm_min, time_infos->tm_sec,
-            action, size, ptr);
+    strftime(time_buffer, 80, "[%B %d %Y][%H:%M:%S]", time_infos);
+    fprintf(log_file, "%s %s %ld bytes at address %p\n", time_buffer, action, size, ptr);
     fclose(log_file);
 }
 
+/**
+ * @brief Function to extract, the most important info from the log
+ *
+ */
 void read_log(void)
 {
     FILE *log_file = fopen("log", "r");
     int ret = 1;
     size_t size;
     char adress[14];
-    char trash[30];
+    char trash[32];
 
     if (log_file == NULL)
     {
@@ -47,8 +61,8 @@ void read_log(void)
     }
     while (ret != EOF)
     {
-        ret = fscanf(log_file, "%s %s %lu %s %s %s %s\n",
-                     trash, trash, &size,
+        ret = fscanf(log_file, "%s %s %s %s %lu %s %s %s %s\n",
+                     trash, trash, trash, trash, &size,
                      trash, trash, trash, adress);
         if (ret != EOF)
             printf("%s %ld bytes\n", adress, size);
